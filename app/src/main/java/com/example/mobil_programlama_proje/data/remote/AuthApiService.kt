0c4cfe8f9845f741a4a90e5dbad723ec0b3fe00b
memirhantasci <220202092@kocaeli.edu.tr>
2025-12-26 23:32:27 +0300

    feat: login altyapısı, viewmodel ve compose ekranı eklendi

--- app/src/main/java/com/example/mobil_programlama_proje/data/remote/AuthApiService.kt
+++ app/src/main/java/com/example/mobil_programlama_proje/data/remote/AuthApiService.kt
@@ -1,5 +1,7 @@
 package com.example.mobil_programlama_proje.data.remote
 
+import com.example.mobil_programlama_proje.data.model.AuthResponse
+import com.example.mobil_programlama_proje.data.model.LoginRequest
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
